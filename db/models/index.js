9fe240312fd4de7090c5edba61d8921870d05daa
naa-7 <nalawad000@citymail.cuny.edu>
2023-06-30 01:22:02 -0400

    update the tables relationship

--- db/models/index.js
+++ db/models/index.js
@@ -2,9 +2,9 @@ const Student = require("./student.js");
 const Campus = require("./campus.js");
 
 
-// Our Associations: One to One (1:1) & Many to Many (M:N)
-Student.hasOne(Campus);//, { through: "ShopperShoes" });
-Campus.hasMany(Student);//, { through: "ShopperShoes" });
+// Our Associations: one-to-many
+Campus.hasMany(Student);
+Student.belongsTo(Campus);
 
 
 module.exports = {
