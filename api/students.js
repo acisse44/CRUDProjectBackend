e1de944da48d66abab8a594e77f5fcff346a039f
9Mohamedismail <87681402+9Mohamedismail@users.noreply.github.com>
2023-06-30 17:23:04 -0400

    Added edit to Campus. Added add, delete, edit to Student.

--- api/students.js
+++ api/students.js
@@ -15,17 +15,64 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const studentID = req.params.id;
+    console.log(studentID);
+    const student = await Student.findByPk(studentID);
+
+    if (!student) {
+      res.status(404).send("Student not found");
+    } else {
+      await student.destroy();
+      res.status(200).send("Student deleted successfully");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/:id", async (req, res, next) => { 
-    try {
-      const studentId = req.params.id; //retreive the student id first
-      const student = await Student.findByPk(studentId); //fetching a single student from database
-      student
-        ? res.status(200).json(student)
-        : res.status(404).send("Student Not Found");
-    } catch (error) {
-      next(error);
+  try {
+    const studentId = req.params.id; //retreive the student id first
+    const student = await Student.findByPk(studentId); //fetching a single student from database
+    student
+      ? res.status(200).json(allStudents)
+      : res.status(404).send("Student Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// ERROR: duplicate keys can be made
+router.post("/", async (req, res, next) => {
+  try {
+    const results = Student.create(req.body);
+    if (!results) {
+      res.status(400).send("Failed to create Student");
+    } else {
+      res.status(200).send("Student added successfully");
     }
-  });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/", async (req, res, next) => {
+  try {
+    const studentId = req.body.id;
+
+    const student = await Student.findByPk(studentId);
+
+    if (!student) {
+      res.status(400).send("Failed to update Student");
+    } else {
+      await student.update(req.body);
+      res.status(200).send("Student updated successfully");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
