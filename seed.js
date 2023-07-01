ceac285b4f5f7b04917dc80c390534cd617a93d0
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-01 00:43:41 -0400

    add campusId to seedStudents to create FK and reorgnize file

--- seed.js
+++ seed.js
@@ -1,13 +1,38 @@
 const db = require("./db");
 const { Student, Campus } = require("./db/models");
 
+const seedCampuses = [
+  {   
+    name: "Brooklyn College", 
+    imageUrl: "https://www.brooklyn.edu/wp-content/uploads/NEWS-Default-1-Featured.jpg",
+    description: "Brooklyn College Campus", 
+    address: "2900 Bedford Ave, Brooklyn, NY 11210" 
+  },
+
+  {   
+    name: "City College", 
+    imageUrl: "https://www.harlemonestop.com/images/organizations/1542.jpg?v=1587326290",
+    description: "City College Campus", 
+    address: "160 Convent Ave, New York, NY 10031" 
+  },
+
+ {  
+    name: "Queens College", 
+    imageUrl: "https://qns.com/wp-content/uploads/2020/11/twitter_sm-07.png",
+    description: "Queens College Campus", 
+    address: "65-30 Kissena Blvd, Queens, NY 11367" 
+  },
+
+];
+
 const seedStudents = [
       { 
         firstName: "Henry", 
         lastName: "Danger", 
-        imageUrl: "https://static.wikia.nocookie.net/heroes-and-villain/images/9/98/Henry_Hart_%28Kid_Danger%29Image.jpg/revision/latest?cb=20201209152619", 
+        imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.drNoWz_I5fraAz1voYt4cAHaEK%26pid%3DApi&f=1&ipt=f9ba176e780e0baeb236bc41028313135c21e8aba6953bf43039fe6511aec79d&ipo=images", 
         email: "henry12@123.com", 
-        gpa: 3.9 
+        gpa: 3.9,
+        campusId: 2,
       },
 
       {       
@@ -15,7 +40,9 @@ const seedStudents = [
         lastName: "Man",    
         imageUrl: "https://insomniac.games/wp-content/uploads/2018/09/Spider-Man_PS4_Selfie_Photo_Mode_LEGAL.jpg", 
         email: "spider22@123.com", 
-        gpa: 3.0 
+        gpa: 3.0,
+        campusId: 1,
+
       },
 
       {     
@@ -23,38 +50,16 @@ const seedStudents = [
         lastName: "Panther", 
         imageUrl: "https://media.newyorker.com/photos/5a875e3f33aebd0cab9bab12/1:1/w_1079,h_1079,c_limit/Brody-Passionate-Politics-Black-Panther.jpg", 
         email: "wakanda1@123.com", 
-        gpa: 4.0 
+        gpa: 4.0,
+        campusId: 3,
+
       },
 ];
-
-const seedCampuses = [
-      {   
-        name: "Brooklyn College", 
-        imageUrl: "https://www.brooklyn.edu/wp-content/uploads/NEWS-Default-1-Featured.jpg",
-        description: "Brooklyn College Campus", 
-        address: "2900 Bedford Ave, Brooklyn, NY 11210" 
-      },
-
-      {   
-        name: "City College", 
-        imageUrl: "https://www.harlemonestop.com/images/organizations/1542.jpg?v=1587326290",
-        description: "City College Campus", 
-        address: "160 Convent Ave, New York, NY 10031" 
-      },
-
-     {  
-        name: "Queens College", 
-        imageUrl: "https://qns.com/wp-content/uploads/2020/11/twitter_sm-07.png",
-        description: "Queens College Campus", 
-        address: "65-30 Kissena Blvd, Queens, NY 11367" 
-      },
-
-  ];
   
 const seed = async () => {
     try {
-      await Student.bulkCreate(seedStudents);
       await Campus.bulkCreate(seedCampuses);
+      await Student.bulkCreate(seedStudents);
       console.log("Seeding completed successfully.");
     } catch (error) {
       console.error("Seeding failed:", error);
