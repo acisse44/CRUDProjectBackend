0f2bb8cb23ddf99edac71f5f5407e74a91d88976
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-02 14:53:01 -0400

    added foreign key constraint

--- db/models/index.js
+++ db/models/index.js
@@ -3,7 +3,7 @@ const Campus = require("./campus.js");
 
 
 // Our Associations: one-to-many
-Campus.hasMany(Student, { foreignKey: "campusId" });
+Campus.hasMany(Student, { foreignKey: "campusId", onDelete: "CASCADE" });
 Student.belongsTo(Campus, { foreignKey: "campusId" });
 
 module.exports = {
