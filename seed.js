aaa1696e7cad242eeb89efa4d3d4c9f03326630e
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-02 14:53:31 -0400

    changes campusId

--- seed.js
+++ seed.js
@@ -32,7 +32,7 @@ const seedStudents = [
         imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.drNoWz_I5fraAz1voYt4cAHaEK%26pid%3DApi&f=1&ipt=f9ba176e780e0baeb236bc41028313135c21e8aba6953bf43039fe6511aec79d&ipo=images", 
         email: "henry12@123.com", 
         gpa: 3.9,
-        campusId: 2,
+        campusId: 28,
       },
 
       {       
@@ -41,7 +41,7 @@ const seedStudents = [
         imageUrl: "https://insomniac.games/wp-content/uploads/2018/09/Spider-Man_PS4_Selfie_Photo_Mode_LEGAL.jpg", 
         email: "spider22@123.com", 
         gpa: 3.0,
-        campusId: 1,
+        campusId: 29,
 
       },
 
@@ -51,7 +51,7 @@ const seedStudents = [
         imageUrl: "https://media.newyorker.com/photos/5a875e3f33aebd0cab9bab12/1:1/w_1079,h_1079,c_limit/Brody-Passionate-Politics-Black-Panther.jpg", 
         email: "wakanda1@123.com", 
         gpa: 4.0,
-        campusId: 3,
+        campusId: 30,
 
       },
 ];
